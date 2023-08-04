0aea289ee45fda754cda6e4604859f39845ac06b
unknown <ksuleymanli99@gmail.com>
2023-08-04 14:36:29 +0300

    Function;car model;coupble and single number

---
+++ assets/Js/main.js
@@ -0,0 +1,48 @@
+function addNumber (a, b){
+
+    console.log(a, b);
+}
+addNumber(5, 7)
+addNumber(4, 9)
+
+
+function addPlus(a, b){
+    console.log(a+ b)
+}
+
+addPlus(9, 19)
+
+
+class Car{
+    constructor(name,color,year,box){
+    this.name=name
+    this.color=color
+    this.year=year
+    this.box=box
+}
+}
+
+const marka=new Car("Prius", "red","1999", "1.6")
+ 
+console.log(marka)
+
+
+
+{
+    let a=20;
+    let b=5;
+    console.log(a/b)
+
+}
+
+{
+    let a=20;
+    let b=2;
+    a>=10?   console.log("Couple") : console.log("Single");
+}
+
+{
+    let a=8
+    let b=2
+    a===0.5? console.log("Tek") : console.log("Cut")
+ }
